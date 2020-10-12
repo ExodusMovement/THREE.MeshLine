527a5f3da2d92a15d1e919f744eb8b30a974e5cc
Vodopyanov Egor <egorvodopyanov@gmail.com>
2020-10-12 16:05:17 +0700

    comment out raycast

--- src/THREE.MeshLine.js
+++ src/THREE.MeshLine.js
@@ -1,16 +1,16 @@
 import { BufferGeometry } from 'three/src/core/BufferGeometry'
 import { BufferAttribute } from 'three/src/core/BufferAttribute'
-import { Matrix4 } from 'three/src/math/Matrix4'
-import { Ray } from 'three/src/math/Ray'
-import { Sphere } from 'three/src/math/Sphere'
 import { Vector3 } from 'three/src/math/Vector3'
-import { LineSegments } from 'three/src/objects/LineSegments'
 import { ShaderChunk } from 'three/src/renderers/shaders/ShaderChunk'
 import { ShaderMaterial } from 'three/src/materials/ShaderMaterial'
 import { UniformsLib } from 'three/src/renderers/shaders/UniformsLib'
 import { Geometry } from 'three/src/core/Geometry'
 import { Color } from 'three/src/math/Color'
 import { Vector2 } from 'three/src/math/Vector2'
+
+// raycast
+// import { Matrix4 } from 'three/src/math/Matrix4'
+// import MeshLineRaycast from './mesh-line-raycast'
 
 function MeshLine () {
   BufferGeometry.call(this)
@@ -31,7 +31,7 @@ function MeshLine () {
   this.widthCallback = null
   
   // Used to raycast
-  this.matrixWorld = new Matrix4()
+  // this.matrixWorld = new Matrix4()
   
   Object.defineProperties(this, {
     // this is now a bufferGeometry
@@ -71,9 +71,9 @@ MeshLine.prototype = Object.create(BufferGeometry.prototype)
 MeshLine.prototype.constructor = MeshLine
 MeshLine.prototype.isMeshLine = true
 
-MeshLine.prototype.setMatrixWorld = function (matrixWorld) {
-  this.matrixWorld = matrixWorld
-}
+// MeshLine.prototype.setMatrixWorld = function (matrixWorld) {
+//   this.matrixWorld = matrixWorld
+// }
 
 // setting via a geometry is rather superfluous
 // as you're creating a unecessary geometry just to throw away
@@ -128,80 +128,7 @@ MeshLine.prototype.setPoints = function (points, wcb) {
   this.process()
 }
 
-function MeshLineRaycast (raycaster, intersects) {
-  let inverseMatrix = new Matrix4()
-  let ray = new Ray()
-  let sphere = new Sphere()
-  let interRay = new Vector3()
-  let geometry = this.geometry
-  // Checking boundingSphere distance to ray
-  
-  sphere.copy(geometry.boundingSphere)
-  sphere.applyMatrix4(this.matrixWorld)
-  
-  if (raycaster.ray.intersectSphere(sphere, interRay) === false) {
-    return
-  }
-  
-  inverseMatrix.getInverse(this.matrixWorld)
-  ray.copy(raycaster.ray).applyMatrix4(inverseMatrix)
-  
-  let vStart = new Vector3()
-  let vEnd = new Vector3()
-  let interSegment = new Vector3()
-  let step = this instanceof LineSegments ? 2 : 1
-  let index = geometry.index
-  let attributes = geometry.attributes
-  
-  if (index !== null) {
-    let indices = index.array
-    let positions = attributes.position.array
-    let widths = attributes.width.array
-    
-    for (let i = 0, l = indices.length - 1; i < l; i += step) {
-      let a = indices[i]
-      let b = indices[i + 1]
-      
-      vStart.fromArray(positions, a * 3)
-      vEnd.fromArray(positions, b * 3)
-      let width =
-        widths[Math.floor(i / 3)] != undefined ? widths[Math.floor(i / 3)] : 1
-      let precision =
-        raycaster.params.Line.threshold + (this.material.lineWidth * width) / 2
-      let precisionSq = precision * precision
-      
-      let distSq = ray.distanceSqToSegment(
-        vStart,
-        vEnd,
-        interRay,
-        interSegment
-      )
-      
-      if (distSq > precisionSq) continue
-      
-      interRay.applyMatrix4(this.matrixWorld) //Move back to world space for distance calculation
-      
-      let distance = raycaster.ray.origin.distanceTo(interRay)
-      
-      if (distance < raycaster.near || distance > raycaster.far) continue
-      
-      intersects.push({
-        distance: distance,
-        // What do we want? intersection point on the ray or on the segment??
-        // point: raycaster.ray.at( distance ),
-        point: interSegment.clone().applyMatrix4(this.matrixWorld),
-        index: i,
-        face: null,
-        faceIndex: null,
-        object: this,
-      })
-      // make event only fire once
-      i = l
-    }
-  }
-}
-
-MeshLine.prototype.raycast = MeshLineRaycast
+// MeshLine.prototype.raycast = MeshLineRaycast
 MeshLine.prototype.compareV3 = function (a, b) {
   let aa = a * 6
   let ab = b * 6
@@ -720,5 +647,5 @@ MeshLineMaterial.prototype.copy = function (source) {
 export {
   MeshLineMaterial,
   MeshLine,
-  MeshLineRaycast
+  // MeshLineRaycast
 }
---
+++ src/mesh-line-raycast.js
@@ -0,0 +1,80 @@
+import { Matrix4 } from 'three/src/math/Matrix4'
+import { Ray } from 'three/src/math/Ray'
+import { Sphere } from 'three/src/math/Sphere'
+import { Vector3 } from 'three/src/math/Vector3'
+import { LineSegments } from 'three/src/objects/LineSegments'
+
+function MeshLineRaycast (raycaster, intersects) {
+  let inverseMatrix = new Matrix4()
+  let ray = new Ray()
+  let sphere = new Sphere()
+  let interRay = new Vector3()
+  let geometry = this.geometry
+  // Checking boundingSphere distance to ray
+  
+  sphere.copy(geometry.boundingSphere)
+  sphere.applyMatrix4(this.matrixWorld)
+  
+  if (raycaster.ray.intersectSphere(sphere, interRay) === false) {
+    return
+  }
+  
+  inverseMatrix.getInverse(this.matrixWorld)
+  ray.copy(raycaster.ray).applyMatrix4(inverseMatrix)
+  
+  let vStart = new Vector3()
+  let vEnd = new Vector3()
+  let interSegment = new Vector3()
+  let step = this instanceof LineSegments ? 2 : 1
+  let index = geometry.index
+  let attributes = geometry.attributes
+  
+  if (index !== null) {
+    let indices = index.array
+    let positions = attributes.position.array
+    let widths = attributes.width.array
+    
+    for (let i = 0, l = indices.length - 1; i < l; i += step) {
+      let a = indices[i]
+      let b = indices[i + 1]
+      
+      vStart.fromArray(positions, a * 3)
+      vEnd.fromArray(positions, b * 3)
+      let width =
+        widths[Math.floor(i / 3)] != undefined ? widths[Math.floor(i / 3)] : 1
+      let precision =
+        raycaster.params.Line.threshold + (this.material.lineWidth * width) / 2
+      let precisionSq = precision * precision
+      
+      let distSq = ray.distanceSqToSegment(
+        vStart,
+        vEnd,
+        interRay,
+        interSegment
+      )
+      
+      if (distSq > precisionSq) continue
+      
+      interRay.applyMatrix4(this.matrixWorld) //Move back to world space for distance calculation
+      
+      let distance = raycaster.ray.origin.distanceTo(interRay)
+      
+      if (distance < raycaster.near || distance > raycaster.far) continue
+      
+      intersects.push({
+        distance: distance,
+        // What do we want? intersection point on the ray or on the segment??
+        // point: raycaster.ray.at( distance ),
+        point: interSegment.clone().applyMatrix4(this.matrixWorld),
+        index: i,
+        face: null,
+        faceIndex: null,
+        object: this,
+      })
+      // make event only fire once
+      i = l
+    }
+  }
+}
+
+export default MeshLineRaycast
